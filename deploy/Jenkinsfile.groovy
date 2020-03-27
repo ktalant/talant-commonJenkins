35a03b2c10851f2a93f1ba5421430ca3c210a77f
Talant Asan <uchkunfan@gmail.com>
2020-03-27 19:34:23 -0400

    else condition has been added

--- deploy/Jenkinsfile.groovy
+++ deploy/Jenkinsfile.groovy
@@ -69,6 +69,9 @@ def slavePodTemplate = """
           if("${TERRAFORM_APPLY}" == "true"){
             println("i will apply changes")
           }
+          else{
+            println("I will not apply")
+          }
         }
 
       }
